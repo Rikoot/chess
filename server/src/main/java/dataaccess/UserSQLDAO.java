ecfe3eb17e6e91385d198f302186e2719ade4085
Rikoot <126274117+Rikoot@users.noreply.github.com>
2026-02-24 15:33:16 -0700

    Passing User tests

--- server/src/main/java/dataaccess/UserSQLDAO.java
+++ server/src/main/java/dataaccess/UserSQLDAO.java
@@ -1,9 +1,11 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserSQLDAO {
@@ -14,23 +16,70 @@ public class UserSQLDAO {
         } catch (DataAccessException e) {
             throw new RuntimeException(e);
         }
-        this.clearDb();
+        this.createDb();
     }
 
     public UserData getUser(String username) {
-        UserData data = new UserData("Temp", "Temp", "Temp");
-
+        UserData data = null;
+        Connection conn = null;
+        try {
+            conn = DatabaseManager.getConnection();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        String statement = "SELECT * FROM Users WHERE Username = ? LIMIT 1;";
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.setString(1, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                return data;
+            }
+            data = new UserData(resultSet.getString("Username"),
+                    resultSet.getString("Password"),
+                    resultSet.getString("Email"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return data;
     }
     public void createUser(String username,
                            String password,
-                           String email) {
+                           String email) throws DataAccessException {
+        String statement = "INSERT INTO Users VALUES (?, ?, ?);";
+        Connection conn = null;
+        try {
+            conn = DatabaseManager.getConnection();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, email);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Taken");
+        }
 
     }
     public void clearDb() {
+        String statement = "DROP TABLE IF EXISTS Users;";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (DataAccessException e) {
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        createDb();
+    }
+
+    private void createDb() {
         String statement = """
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
     Username VARCHAR(255) NOT NULL,
     Password VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL,
