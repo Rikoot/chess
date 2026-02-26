3c222b8c934ee2b85a8d7fe13fe01ec970526682
Rikoot <126274117+Rikoot@users.noreply.github.com>
2026-02-26 13:47:09 -0700

    Create ServerFacade.java

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,54 @@
+package client;
+
+public class ServerFacade {
+
+    public ServerFacade() {
+
+    }
+
+    // logged out commands
+    public void register() {
+
+    }
+
+    public void login() {
+
+    }
+
+    // logged in commands
+    public void create() {
+
+    }
+
+    public void list() {
+
+    }
+
+    public void join() {
+
+    }
+
+    public void observe() {
+
+    }
+
+    public void logout() {
+
+    }
+
+    // generic commands
+    public void quit() {
+
+    }
+
+    public String help() {
+        String helpString = new String();
+        if (true) {
+
+        } else {
+
+        }
+        return helpString;
+    }
+
+}
