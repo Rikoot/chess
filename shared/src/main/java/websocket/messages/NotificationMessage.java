36245e3a0c8b5017927025980f86af4772ec5ba3
Rikoot <126274117+Rikoot@users.noreply.github.com>
2026-03-30 14:12:59 -0600

    Added ServerMessage classes

---
+++ shared/src/main/java/websocket/messages/NotificationMessage.java
@@ -0,0 +1,34 @@
+package websocket.messages;
+
+import java.util.Objects;
+
+public class NotificationMessage extends ServerMessage{
+    private final String notificationMessage;
+
+    public NotificationMessage(ServerMessageType type, String errorMessage) {
+        super(type);
+        this.notificationMessage = errorMessage;
+    }
+
+    public String getNotificationMessage() {
+        return notificationMessage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        NotificationMessage that = (NotificationMessage) o;
+        return Objects.equals(notificationMessage, that.notificationMessage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), notificationMessage);
+    }
+}
+
